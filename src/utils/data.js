a929bad0aa46703a981e4b46821eca367742e0f5
Luis Ortiz <70501251+KiryuuLight@users.noreply.github.com>
2023-06-09 22:56:43 -0500

    Add character info for use in the card component

---
+++ src/utils/data.js
@@ -0,0 +1,37 @@
+import charImg1 from '../img/characters/char1.png';
+import charImg2 from '../img/characters/char2.png';
+import charImg3 from '../img/characters/char3.png';
+import charImg4 from '../img/characters/char4.png';
+import charImg5 from '../img/characters/char5.png';
+import charImg6 from '../img/characters/char6.png';
+import charImg7 from '../img/characters/char7.png';
+import charImg8 from '../img/characters/char8.png';
+import charImg9 from '../img/characters/char9.png';
+import charImg10 from '../img/characters/char10.png';
+import charImg11 from '../img/characters/char11.png';
+import charImg12 from '../img/characters/char12.png';
+import charImg13 from '../img/characters/char13.png';
+import charImg14 from '../img/characters/char14.png';
+import charImg15 from '../img/characters/char15.png';
+import charImg16 from '../img/characters/char16.png';
+
+const listOfCharacters = [
+  { id: 1, name: 'Yu Narukami', imgSrc: charImg1 },
+  { id: 2, name: 'Yosuke Hanamura', imgSrc: charImg2 },
+  { id: 3, name: 'Chie Satonaka', imgSrc: charImg3 },
+  { id: 4, name: 'Yukiko Amagi', imgSrc: charImg4 },
+  { id: 5, name: 'Kanji Tatsumi', imgSrc: charImg5 },
+  { id: 6, name: 'Rise Kujikawa', imgSrc: charImg6 },
+  { id: 7, name: 'Teddie', imgSrc: charImg7 },
+  { id: 8, name: 'Naoto Shirogane', imgSrc: charImg8 },
+  { id: 9, name: 'Ryotaro Dojima', imgSrc: charImg9 },
+  { id: 10, name: 'Nanako Dojima', imgSrc: charImg10 },
+  { id: 11, name: 'Tohru Adachi', imgSrc: charImg11 },
+  { id: 12, name: 'Sayoko Uehara', imgSrc: charImg12 },
+  { id: 13, name: 'Marie', imgSrc: charImg13 },
+  { id: 14, name: 'Margaret', imgSrc: charImg14 },
+  { id: 15, name: 'Ai Ebihara', imgSrc: charImg15 },
+  { id: 16, name: 'Daisuke Nagase', imgSrc: charImg16 },
+];
+
+export default listOfCharacters;
