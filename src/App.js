7d1ab09ec5a511b38223090bfeaf48f902ffe502
Luis Ortiz <70501251+KiryuuLight@users.noreply.github.com>
2023-06-09 22:57:03 -0500

    Init file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/nav/Header';
+import Gameboard from './components/Gameboard';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Gameboard />
+    </div>
+  );
+}
+
+export default App;
